b82449adfb0a9c4ae70b49fb33c2964f8939bdcc
Rohan James <rohanjames9282@gmail.com>
2023-02-20 14:43:55 +0530

    Add code_name and city_name to airport model

--- db/migrate/20230220075316_create_airports.rb
+++ db/migrate/20230220075316_create_airports.rb
@@ -1,6 +1,8 @@
 class CreateAirports < ActiveRecord::Migration[7.0]
   def change
     create_table :airports do |t|
+      t.string :code_name,   null: false
+      t.string :city_name,   null: false
 
       t.timestamps
     end
