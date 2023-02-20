a08da2ed9439502087a043b3678f026b10ec6e1d
Rohan James <rohanjames9282@gmail.com>
2023-02-20 17:02:14 +0530

    Add associations for arrival and departure airports

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,7 @@
 class Flight < ApplicationRecord
+  belongs_to :arrival_airport, class_name: :airport
+  belongs_to :departure_airport, class_name: :airport
+
+  # validates :duration, presence: true, time
+  # validates :departure_time, presence: true, time
 end
