26f9a27849e9c73d6b7d98dda9750018374badb1
Rohan James <rohanjames9282@gmail.com>
2023-02-20 18:55:38 +0530

    Seed database with flight objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-#To make fresh models when we run rake db:seed
-# Airport.delete_all
-# Flight.delete_all
+#To make fresh models when we run rails db:seed
+Airport.delete_all
+Flight.delete_all
 
 Airport.create!([{
   code_name: "ATL",
@@ -63,5 +63,80 @@ Airport.create!([{
   city_name: "Bao'an",
 }
 ])
+
+Flight.create!([{
+  duration: "2h30m",
+  departure_time: Time.now + 1.day,
+  arrival_airport_id: 3,
+  departure_airport_id: 4,
+},
+{
+  duration: "5h",
+  departure_time: Time.now + 2.days,
+  arrival_airport_id: 4,
+  departure_airport_id: 5,
+},
+{
+  duration: "4h",
+  departure_time: Time.now + 3.days,
+  arrival_airport_id: 5,
+  departure_airport_id: 6,
+},
+{
+  duration: "1h25m",
+  departure_time: Time.now + 4.days,
+  arrival_airport_id: 6,
+  departure_airport_id: 7,
+},
+{
+  duration: "6h30m",
+  departure_time: Time.now + 5.days,
+  arrival_airport_id: 8,
+  departure_airport_id: 9,
+},
+{
+  duration: "18h",
+  departure_time: Time.now + 6.days,
+  arrival_airport_id: 9,
+  departure_airport_id: 10,
+},
+{
+  duration: "21h25m",
+  departure_time: Time.now + 7.days,
+  arrival_airport_id: 10,
+  departure_airport_id: 11,
+},
+{
+  duration: "2h",
+  departure_time: Time.now + 8.days,
+  arrival_airport_id: 11,
+  departure_airport_id: 12,
+},
+{
+  duration: "1h20m",
+  departure_time: Time.now + 9.days,
+  arrival_airport_id: 12,
+  departure_airport_id: 13,
+},
+{
+  duration: "19h45m",
+  departure_time: Time.now + 10.days,
+  arrival_airport_id: 13,
+  departure_airport_id: 14,
+},
+{
+  duration: "8h",
+  departure_time: Time.now + 11.days,
+  arrival_airport_id: 14,
+  departure_airport_id: 15,
+},
+{
+  duration: "10h",
+  departure_time: Time.now + 12.days,
+  arrival_airport_id: 15,
+  departure_airport_id: 16,
+},
+]
+)
 
 
