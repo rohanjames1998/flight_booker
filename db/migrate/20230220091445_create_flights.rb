591eef7d3e0cd79c0986178c74cc4b458038bae8
Rohan James <rohanjames9282@gmail.com>
2023-02-20 17:20:35 +0530

    Add foreign keys for arrival and departure airports

--- db/migrate/20230220091445_create_flights.rb
+++ db/migrate/20230220091445_create_flights.rb
@@ -1,8 +1,15 @@
 class CreateFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :flights do |t|
+      t.time :duration
+      t.datetime :departure_time
 
       t.timestamps
     end
+    add_reference :flights, :arrival_airport, references: :airports, index: true
+    add_reference :flights, :departure_airport, references: :airports, index: true
+
+    add_foreign_key :flights, :airports, column: :arrival_airport_id
+    add_foreign_key :flights, :airports, column: :departure_airport_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_075316) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_091445) do
   create_table "airports", force: :cascade do |t|
     t.string "code_name", null: false
     t.string "city_name", null: false
@@ -18,4 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_075316) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.time "duration"
+    t.datetime "departure_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "arrival_airport_id"
+    t.integer "departure_airport_id"
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
