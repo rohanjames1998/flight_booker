508cc473c4771e6187991986a7cf14b1486519e8
Rohan James <rohanjames9282@gmail.com>
2023-02-20 13:30:57 +0530

    Add associations for arriving and departing flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,5 @@
 class Airport < ApplicationRecord
+  has_many :arriving_flights, foreign_key: :arrival_airport_id, class_name: :flight, dependent: :destroy
+  has_many :departing_flights, foreign_key: :departure_airport_id, class_name: :flight, dependent: :destroy
+
 end
