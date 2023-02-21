3e6b7ca4d6e4d5c4c0ad35853f9a35101a1b725d
Rohan James <rohanjames9282@gmail.com>
2023-02-21 19:13:13 +0530

    Add flight search form on index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @flights = Flight.all.includes(:arrival_airport, :departure_airport)
+    @arrival_airport_options = @flights.map { |f| [f.arrival_airport.code_name, f.arrival_airport.id] }
+    @departure_airport_options = @flights.map { |f| [f.departure_airport.code_name, f.departure_airport.id] }
+    @departure_date = @flights.map { |f| [f.departure_time.strftime("%Y/%b/%d"), f.departure_time] }
   end
 end
