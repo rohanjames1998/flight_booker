8352f1fc182cc8167f20a1b52bac45c39de2a75d
Rohan James <rohanjames9282@gmail.com>
2023-02-21 16:30:38 +0530

    Fix FOREIGN KEY constraint error when deleting airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,7 +1,14 @@
 class Airport < ApplicationRecord
-  has_many :arriving_flights, foreign_key: :arrival_airport_id, class_name: "Flight", dependent: :destroy
-  has_many :departing_flights, foreign_key: :departure_airport_id, class_name: "Flight", dependent: :destroy
+  has_many :arriving_flights, foreign_key: :arrival_airport_id, class_name: "Flight", dependent: :delete_all
+  has_many :departing_flights, foreign_key: :departure_airport_id, class_name: "Flight", dependent: :delete_all
+  before_destroy :destroy_all_flights
 
   validates :code_name, presence: true, length: { in: 3..4 }
   validates :city_name, presence: true, length: { maximum: 25 }
+
+  private
+    def destroy_all_flights
+      self.arriving_flights.destroy_all
+      self.departing_flights.destroy_all
+    end
 end
