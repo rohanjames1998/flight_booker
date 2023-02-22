1c7b4741abb18015c5d8d1dce4ea06b1174324de
Rohan James <rohanjames9282@gmail.com>
2023-02-22 15:39:59 +0530

    Add flight_details method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,8 @@ class Flight < ApplicationRecord
 
   validates :duration, presence: true
   validates :departure_time, presence: true
+
+  def flight_details
+    details = "From #{departure_airport.code_name} -> To #{arrival_airport.code_name}, On #{departure_time.strftime("%Y/%m/%d")} #{departure_time.strftime("%H:%M")} UTC, Flight Duration = #{duration}"
+  end
 end
