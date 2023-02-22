178be056b035d70916b014db2658fa7d25ab93a0
Rohan James <rohanjames9282@gmail.com>
2023-02-22 15:41:28 +0530

    Update #index to check for available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,6 @@ class FlightsController < ApplicationController
     @arrival_airport_options = @flights.map { |f| [f.arrival_airport.code_name, f.arrival_airport.id] }
     @departure_airport_options = @flights.map { |f| [f.departure_airport.code_name, f.departure_airport.id] }
     @departure_date = @flights.map { |f| [f.departure_time.strftime("%Y/%b/%d"), f.departure_time] }
+    @available_flights = Flight.where(arrival_airport_id: params[:flight][:destination_airport_id], departure_airport_id: params[:flight][:departure_airport_id]) if params[:flight]
   end
 end
