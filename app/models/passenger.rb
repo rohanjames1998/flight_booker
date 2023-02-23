e4d4c7447f7c6769a268a59d8447b24b736b88b6
Rohan James <rohanjames9282@gmail.com>
2023-02-23 13:00:37 +0530

    Add validations

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :bookings
+  has_many :flights, through: :bookings
+
+  validates :name, presence: true, length: { in: 3..20 }
+  validates :email, presence: true, length: { in: 8..20 }
 end
