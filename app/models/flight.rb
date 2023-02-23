00f77eb2b00c8b4aa450ee4d1fbe0f34df006d80
Rohan James <rohanjames9282@gmail.com>
2023-02-23 13:02:54 +0530

    Add association between flights, bookings, and passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
+  has_many :bookings
+  has_many :passengers, through: :bookings
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
 
