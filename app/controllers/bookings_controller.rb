77bc94c338a97f0659bba0ef70de03ad59730c16
Rohan James <rohanjames9282@gmail.com>
2023-02-23 17:15:48 +0530

    Add logic to show flight details when creating new bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,9 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
   end
+
+  # def create
+  #   @booking = 
 end
