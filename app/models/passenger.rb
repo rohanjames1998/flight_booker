1aeffd0ee2748d3b6262941bc5ba4ee11d59729f
Rohan James <rohanjames9282@gmail.com>
2023-02-25 15:41:39 +0530

    Fix pluralization for association

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,6 @@
 class Passenger < ApplicationRecord
   has_many :flights, through: :bookings
-  belongs_to :bookings
+  belongs_to :booking
 
   validates :name, presence: true, length: { in: 3..20 }
   validates :email, presence: true, length: { in: 8..20 }
