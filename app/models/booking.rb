03f0776f7424d20c717fe307e85fae7a1239ef03
Rohan James <rohanjames9282@gmail.com>
2023-02-25 15:40:18 +0530

    Remove rejection for passenger attributes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   has_many :passengers
   belongs_to :flight
 
-  accepts_nested_attributes_for :passengers, reject_if: ->(attributes){ attributes['name'].blank? || attributes['email'].blank? }
+  accepts_nested_attributes_for :passengers
 end
