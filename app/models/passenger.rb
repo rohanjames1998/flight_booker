316e202fe5018d0eecec7a5e91eb5dedf35caa24
Rohan James <rohanjames9282@gmail.com>
2023-03-07 17:31:36 +0530

    Fix association between flights and passenger through booking

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,6 @@
 class Passenger < ApplicationRecord
-  has_many :flights, through: :bookings
   belongs_to :booking
+  has_many :flights, through: :booking
 
   validates :name, presence: true, length: { in: 3..20 }
   validates :email, presence: true, length: { in: 8..20 }
