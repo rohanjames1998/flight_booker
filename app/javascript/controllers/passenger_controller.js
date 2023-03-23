c0a714c17ece19a755090f31d2a42929684ab22f
Rohan <rohanjames9282@gmail.com>
2023-03-23 14:59:27 +0530

    Fix users being able to remove all passengers

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'passengerForm', 'parentDiv' ]
+  static targets = [ 'passengerForm', 'parentDiv', 'passengerAdditionWarning', 'passengerRemovalWarning'  ];
 
   connect() {
     this.addPassenger();
@@ -9,14 +9,26 @@ export default class extends Controller {
 
   addPassenger() {
     const randomIndex = new Date().getTime();
-    const passengerForm = this.passengerFormTarget.innerHTML.replace(/PASSENGER_RECORD/g, randomIndex)
+    const passengerForm = this.passengerFormTarget.innerHTML.replace(/PASSENGER_RECORD/g, randomIndex);
     const parentDiv = this.parentDivTarget;
     parentDiv.insertAdjacentHTML('beforeend', passengerForm);
   }
 
   removePassenger(e){
-    const passenger = e.target.closest("#passenger-form");
-    passenger.querySelector("input[name*='_destroy']").value = 1
-    passenger.hidden = true;
+    const passenger = e.target.closest("fieldset");
+    if ( this.visiblePassengerFields() > 1){
+      passenger.querySelector("input[name*='_destroy']").value = 1
+      passenger.hidden = true;
+      this.passengerRemovalWarningTarget.textContent = ""
+    }
+    else {
+      this.passengerRemovalWarningTarget.textContent = "Flight Booking Need At Least One Passenger."
+    }
+  }
+
+  visiblePassengerFields() {
+   const visibleFields = Array.from(document.querySelectorAll('fieldset')).filter(s =>
+      window.getComputedStyle(s).getPropertyValue('display') != 'none');
+      return visibleFields.length
   }
 }
