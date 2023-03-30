2d045ab27a9166590373c2b84df9f8c5d29d361d
Rohan James <rohanjames9282@gmail.com>
2023-03-30 14:29:31 +0530

    Add #arrival_time to flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -10,4 +10,13 @@ class Flight < ApplicationRecord
   def flight_details
     details = "From #{departure_airport.code_name} -> To #{arrival_airport.code_name}, On #{departure_time.strftime("%Y/%m/%d")} #{departure_time.strftime("%H:%M")} UTC, Flight Duration = #{duration}"
   end
+
+  def arrival_time
+    # Since duration is stored as a string and not a dateTime object we first
+    # convert it to a Time object.
+    parsed_duration = Time.parse(duration)
+    travel_hours = parsed_duration.hour
+    travel_mins = parsed_duration.min
+    arrival_time = departure_time + travel_hours.hours + travel_mins.minutes
+  end
 end
