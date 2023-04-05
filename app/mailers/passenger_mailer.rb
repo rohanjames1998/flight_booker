83e4da9641c37b45cf60fa196bda45ff37e7b48c
Rohan James <rohanjames9282@gmail.com>
2023-04-05 14:52:13 +0530

    Add #confirmation_mail

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'admin@foobarlines.com'
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @arrival_airport = params[:arrival_airport]
+    mail(to: @passenger.email, subject: "You flight has been confirmed")
+  end
 end
